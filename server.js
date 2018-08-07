32a637906173923a6af38703a2afe85589068cbf
russiaaiyappa <russia.ayya@gmail.com>
2018-08-07 01:19:24 -0400

    created user, section and enrollment models, schema and services

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/webdev-summer2-2018');
+
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 var session = require('express-session')
 app.use(session({
@@ -33,5 +51,10 @@ function getSession(req, res) {
     var value = req.session[name];
     res.send(value);
 }
+
+var userService = require('./services/user.service.server');
+userService(app);
+
+require('./services/section.service.server')(app);
 
 app.listen(4000);
